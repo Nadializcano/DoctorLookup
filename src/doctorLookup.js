d0f0a156034d4d7107075bf02efd42b2322c5988
Nadia Lizcano <priscila.lizcano@gmail.com>
2019-06-11 10:21:11 -0700

    edit apikey

--- src/doctorLookup.js
+++ src/doctorLookup.js
@@ -3,7 +3,7 @@ export class DoctorLookup {
 
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${medicalIssue}&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=3baf33df42f32256091e2ddad3e63f0b`;
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${medicalIssue}&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${process.env.apiKey}`;
 
       request.onload = function() {
         if (this.status === 200) {
